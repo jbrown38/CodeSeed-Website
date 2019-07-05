1ac467294e4d482f1962537740e895b43ab2f5be
Dane Williams <dwilli36@nd.edu>
2019-07-04 18:41:59 -0700

    navbar animations

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict';
+var app = angular.module("CodeSeed", ['ngRoute']);
+
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'home.html',
+    controller: 'homeCtrl'
+  });
+
+  $routeProvider.when('/WhatWeDo', {
+    templateUrl: 'WhatWeDo.html',
+    controller: 'WhatWeDoCtrl'
+  });
+
+  $routeProvider.when('/WhoWeAre', {
+    templateUrl: 'WhoWeAre.html',
+    controller: 'WhoWeAreCtrl'
+  });
+
+  $routeProvider.when('/HowWeThink', {
+    templateUrl: 'HowWeThink.html',
+    controller: 'HowWeThinkCtrl'
+  });
+
+  $routeProvider.when('/LetsTalk', {
+    templateUrl: 'LetsTalk.html',
+    controller: 'LetsTalkCtrl'
+  });
+  
+  $routeProvider.otherwise({redirecTo: '/'});
+}]);
+
+
+app.controller('homeCtrl', ['$scope', function($scope){
+    $scope.message = 'welcome to home!';
+}]);
+
+app.controller('WhatWeDoCtrl', ['$scope', function($scope){
+    $scope.message = '';
+}]);
+
+app.controller('WhoWeAreCtrl', ['$scope', function($scope){
+    $scope.message = '';
+}]);
+
+app.controller('HowWeThinkCtrl', ['$scope', function($scope) {
+    $scope.message = '';
+}]);
+
+app.controller('LetsTalkCtrl', ['$scope', function($scope){
+    $scope.message = '';
+}]);
